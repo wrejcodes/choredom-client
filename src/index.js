b3535c97382cdcd998f394edf13e7dec7c3074d1
Will Jackson <wrejcodes@gmail.com>
2017-09-28 14:05:22 -0500

    some more default configuration stuff for redux and react-router

--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import {BrowserRouter as Router} from 'react-router-dom';
+import appReducer from './reducers/'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// here we create the store for redux from our combined reducers - Will
+
+const store = createStore(appReducer);
+
+ReactDOM.render(
+	// here we set up the router for react-router to work properly for client side routing
+	// we also set the store for redux and pass it to the Provider - Will
+	<Provider store={store}>
+		<Router>
+			<App />
+		</Router>
+	</Provider>, document.getElementById('root'));
 registerServiceWorker();
