06a42fa3ccfdc7128ec3569dc7d4e8478511c335
Will Jackson <wrejcodes@gmail.com>
2017-09-28 14:08:53 -0500

    I'm actually adding the files that weren't tracked this time :neutral_face:

---
+++ src/actions/index.js
@@ -0,0 +1,4 @@
+// This will be our main actions page we might end up splitting these up later
+// for now just put all of our actions in this file
+// we should export each action object creator here as well - Will
+
---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+// we need to import other reducers here
+import { combineReducers } from 'redux';
+
+
+const appReducer = combineReducers({
+	// each reducer that we import should be here
+
+});
+
+export default appReducer;
