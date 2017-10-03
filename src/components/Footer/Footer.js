a362298e35b480bf6f84903186a4eec7a559f15c
Will Jackson <wrejcodes@gmail.com>
2017-10-03 14:45:07 -0500

    updated Footer to use dynamic year date

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,9 +5,10 @@ class Footer extends Component{
 
 	render(){
 		return(
-			// using bootstrap navbar-fixed-bottom to keep the footer at the bottom
+			// using bootstrap navbar-fixed-bottom to keep the footer at the bottom - Will
+			// Date().getFullYear() for minimizing upkeep on footer
 			<footer className="navbar-fixed-bottom Footer-container">
-				&copy; Group 6 2017
+				&copy; Group 6 { new Date().getFullYear() } 
 			</footer>
 		);
 	}
