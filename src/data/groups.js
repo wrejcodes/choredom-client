b14b90eec3b5c7b0c72a1756f6d883711ce1cd55
Sean T. Purvis <Sean.Purvis@usm.edu>
2017-10-04 12:31:16 -0500

    Update Groups to Contain UserIDs

--- src/data/groups.js
+++ src/data/groups.js
@@ -2,22 +2,29 @@ const groups = [
   {
     id: 1,
     name: "Group Alpha",
-    userIDs: [],
+    userIDs: [1, 2, 4],
     adminID: 1,
   },
 
   {
     id: 2,
     name: "Group Beta",
-    userIDs: [],
-    adminID: 1,
+    userIDs: [1, 3, 4],
+    adminID: 2,
   },
 
   {
     id: 3,
     name: "Group Gamma",
-    userIDs: [],
-    adminID: 0,
+    userIDs: [1, 2, 3],
+    adminID: 3,
+  },
+
+  {
+    id: 4,
+    name: "Group Delta",
+    userIDS: [2, 3, 4],
+    adminID: 4
   },
 ]
 export default groups;
