f2531bba386ef673def5a5834751c1c82d1ec6c9
Sean T. Purvis <Sean.Purvis@usm.edu>
2017-10-04 11:03:03 -0500

    Create im Objects

---
+++ data/users.js
@@ -0,0 +1,21 @@
+export default const users = [
+  {
+    id: 1,
+    name: "Will",
+  },
+
+  {
+    id: 2,
+    name: "Sean"
+  },
+
+  {
+    id: 3,
+    name: "Sarju"
+  },
+
+  {
+    id: 4,
+    name: "Eric",
+  }
+]
