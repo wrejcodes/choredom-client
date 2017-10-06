fa54fd7a9d52de7517f21580474edeba56c6f49e
Will Jackson <wrejcodes@gmail.com>
2017-10-06 01:23:29 -0500

    added some actions for removing, stealing, and buying out chores. laptop is almost dead :sad:

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,10 @@
 export const ADD_FLASH_MESSAGE = 'ADD_FLASH_MESSAGE';
 export const REMOVE_FLASH_MESSAGE = 'REMOVE_FLASH_MESSAGE';
+export const STEAL_CHORE = 'STEAL_CHORE';
+export const BUY_OUT_CHORE = 'BUY_OUT_CHORE';
+export const REMOVE_CHORE = 'REMOVE_CHORE';
+
+
 let message_id = 0;
 
 // here we list all of our action creators
@@ -20,3 +25,19 @@ export const remove_flash_message = (id) =>(
 		id
 	}
 );
+
+export const steal_chore = (choreId, userId) =>(
+	{
+		type: STEAL_CHORE,
+		choreId,
+		userId
+	}
+);
+
+export const buy_out_chore = (choreId, userId) =>{
+	type: BUY_OUT_CHORE,
+	choreId,
+	userId
+}
+
+
