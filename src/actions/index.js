8382222520be241dcbb243053d41e331225bfc84
Will Jackson <wrejcodes@gmail.com>
2017-10-06 07:01:31 -0500

    added some action creators and reducers for handling adding points, removing points, and reassining/completing chores

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,8 @@ export const REMOVE_FLASH_MESSAGE = 'REMOVE_FLASH_MESSAGE';
 export const STEAL_CHORE = 'STEAL_CHORE';
 export const BUY_OUT_CHORE = 'BUY_OUT_CHORE';
 export const REMOVE_CHORE = 'REMOVE_CHORE';
+export const ADD_POINTS = 'ADD_POINTS';
+export const REMOVE_POINTS = 'REMOVE_POINTS';
 
 
 let message_id = 0;
@@ -26,18 +28,37 @@ export const remove_flash_message = (id) =>(
 	}
 );
 
-export const steal_chore = (choreId, userId) =>(
+export const steal_chore = (choreId, assignTo) =>(
 	{
 		type: STEAL_CHORE,
 		choreId,
-		userId
+		assignTo
 	}
 );
 
-export const buy_out_chore = (choreId, userId) =>{
+export const buy_out_chore = (choreId, assignTo) =>{
 	type: BUY_OUT_CHORE,
 	choreId,
-	userId
+	assignTo
+}
+
+export const remove_chore = (choreId) =>{
+	type: REMOVE_CHORE,
+	choreId
+}
+
+export const add_points = (groupId, userId, points) =>{
+	type: ADD_POINTS,
+	groupId,
+	userId,
+	points
+}
+
+export const remove_points = (groupId, userId, points) =>{
+	type: REMOVE_POINTS,
+	groupId,
+	userId,
+	points
 }
 
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,30 @@
 import { combineReducers} from 'redux';
-import flash_messages from './flash_messages'
+import flash_messages from './flash_messages';
+import {REMOVE_CHORE, STEAL_CHORE, BUY_OUT_CHORE} from '../actions/';
+import {ADD_POINTS, REMOVE_POINTS} from '../actions/';
 
 const groups = (state = [], action)=>{
 	switch(action.type){
+		case ADD_POINTS:
+		return state.map((group)=>{
+			let temp = group;
+			if (group.id === action.groupId){
+				let tempPoints = Object.assign({},group.points);
+				tempPoints[`${action.userId}`] += action.points;
+				temp = Object.assign({},...group, {points: tempPoints}); 
+			}
+			return temp;
+		});
+		case REMOVE_POINTS:
+		return state.map((group)=>{
+			let temp = group;
+			if (group.id === action.groupId){
+				let tempPoints = Object.assign({},group.points);
+				tempPoints[`${action.userId}`] -= action.points;
+				temp = Object.assign({},...group, {points: tempPoints}); 
+			}
+			return temp;
+		});
 		default: return state;
 	}
 };
@@ -13,6 +35,29 @@ const users = (state = [], action)=>{
 };
 const chores = (state = [], action)=>{
 	switch(action.type){
+		case STEAL_CHORE:
+		return state.map((chore)=>{
+			let temp = chore;
+			if (chore.id === action.choreId){
+				let tempPoints = Object.assign({},chore.points);
+				tempPoints.worth = tempPoints.steal;
+				tempPoints.steal = 0;
+				temp = Object.assign({}, ...chore, {belongsToUser: action.assignTo}, {points: tempPoints});
+			}
+			return temp;
+		});
+		case BUY_OUT_CHORE:
+		return state.map((chore)=>{
+			let temp = chore;
+			if (chore.id === action.choreId){
+				temp = Object.assign({},...chore, {belongsToUser: action.assignTo});
+			}
+			return temp;
+		});
+		case REMOVE_CHORE:
+		return state.filter((chore)=>{
+			return chore.id !== action.choreId;
+		});
 		default: return state;
 	}
 };
