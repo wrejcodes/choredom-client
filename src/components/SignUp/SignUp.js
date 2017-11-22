c057089a50ad39a7233d26b79d9efbce54bf96d9
Will Jackson <wrejcodes@gmail.com>
2017-11-21 18:02:29 -0600

    Started basic groundwork for form validation in the signup form

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -4,23 +4,48 @@ import './SignUp.css';
 import {Link} from 'react-router-dom';
 
 class SignUpForm extends Component {
+	constructor(props){
+		super(props);
+		this.validateName = this.validateName.bind(this);
+		this.handleChange = this.handleChange.bind(this);
+		this.state = {firstName: '', lastName:'', username: '', email: '', password: '', confirm: ''};
+	}
+
+	validateName(fieldName){
+		let length = this.state[fieldName].length;
+		if(length === 0){
+			return null;
+		}
+		if(length > 2){
+			return 'success';
+		} else {
+			return 'error';
+		}
+	}
+
+	handleChange(fieldName, value){
+		let stateObj = {};
+		stateObj[fieldName] = value;
+		this.setState(stateObj);
+	}
+
 	render(){
 		return(
 			<Form horizontal className="form-container">
-			<FormGroup controlId="firstName">
+			<FormGroup controlId="firstName" validationState={this.validateName('firstName')}>
 					<Col componentClass={ControlLabel} sm={2} smOffset={3}>
 						First Name
 					</Col>
 					<Col sm={4}>
-						<FormControl type="firstName" placeholder="First Name" />
+						<FormControl type="firstName" placeholder="First Name" onChange={(e)=>{this.handleChange('firstName', e.target.value)}} value={this.state.firstName}  />
 					</Col>
 			</FormGroup>
-			<FormGroup controlId="lastName">
+			<FormGroup controlId="lastName" validationState={this.validateName('lastName')}>
 					<Col componentClass={ControlLabel} sm={2} smOffset={3}>
 						Last Name
 					</Col>
 					<Col sm={4}>
-						<FormControl type="lastName" placeholder="Last Name" />
+						<FormControl type="lastName" placeholder="Last Name" onChange={(e)=>{this.handleChange('lastName', e.target.value)}}/>
 					</Col>
 			</FormGroup>
 
