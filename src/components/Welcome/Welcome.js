aff6351f1455f46d4fe8e8e442caac21881b8dea
Will Jackson <wrejcodes@gmail.com>
2017-12-01 03:27:47 -0600

    Edited styling of main homepage and added images to main homepage

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -1,22 +1,41 @@
 import React, {Component} from 'react';
-import {Col} from 'react-bootstrap';
+import {Col, Row, Image, GlyphIcon} from 'react-bootstrap';
 import LoginForm from '../Login/LoginForm';
 import './Welcome.css';
+import dishes from './dish_washing.jpg';
+import garbage from './garbage.jpg';
+import laundry from './laundry.jpg';
+import cleaning from './cleaning.jpg';
 
 class Welcome extends Component{
 
 	render(){
 		return(
 			<div className="welcome container">
-				<h1>Welcome to Choredom</h1>
-				<Col md={6}>
-					<h3>Choredom makes managing chores easy</h3>
-					<p>Complete chores to earn points</p>
-					<p>Spend points to offload unwanted chores</p>
-				</Col>
-				<Col md={6}>
-					<LoginForm />
-				</Col>
+				<Row className="Login-Row">
+					<span className="centered"><Image src={cleaning} responsive/></span>
+					<small>Free Vector Art by <a rel="nofollow" href="https://www.Vecteezy.com">Vecteezy!</a></	small>
+					<h1 className="Welcome">Welcome to Choredom</h1>
+					<Col md={6} className="Description">
+						<h3>Choredom makes managing chores easy</h3>
+						<h4><i className="fa fa-star" /> Complete chores to earn points. <i className="fa fa-star" /></h4>
+						<h4><i className="fa fa-gift" /> Spend points to offload unwanted chores. <i className="fa fa-gift" /></h4>
+					</Col>
+					<Col md={6}>
+						<LoginForm />
+					</Col>
+				</Row>
+				<Row className="pictureRow">
+					<Col md={2}>
+						<Image src={dishes} responsive rounded/>
+					</Col>
+					<Col md={2}>
+						<Image src={garbage} responsive rounded/>
+					</Col>
+					<Col md={2}>
+						<Image src={laundry} responsive rounded/>
+					</Col>
+				</Row>
 			</div>
 		);
 	}
